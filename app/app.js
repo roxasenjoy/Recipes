a070583ad111c5e853ab7acc56833dfe5cb209bb
roxasenjoy <andrew.mahe14@gmail.com>
2023-05-21 23:28:39 +0200

    second commité

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Le serveur est en écoute sur le port 3000');
+});
